6def1833be88b5b5b60664cc64d9170587472095
Elimane Ba <elimane.ba.email.professionnel@gmail.com>
2022-01-14 09:58:15 +0000

    Ventilo implement

--- src/app.ts
+++ src/app.ts
@@ -3,22 +3,30 @@ import {frigo} from './classes/frigo';
 import {ventilo} from './classes/ventilo';
 import {observe} from './interfaces/observe'
 
-let temps: Temperature= new Temperature (25)
+let temps: Temperature = new Temperature(25) ;
+let nbrVentilo=0;
 
-const ventilo1:observe = new ventilo ('ventilo1')
-const ventilo2 : observe = new ventilo('ventilo2')
-const ventilo3 : observe = new ventilo('ventilo3')
+//  const ventilo1 :  observe = new ventilo ('ventilo1')
+// const ventilo2 : observe = new ventilo('ventilo2')
+// const ventilo3 : observe = new ventilo('ventilo3')
 
 const frigo1 : observe = new frigo('frigo1')
 const frigo2 : observe = new frigo('frigo2')
 
-temps.subscribe(ventilo1)
-temps.subscribe(ventilo2)
-temps.subscribe(ventilo3)
+// temps.subscribe(ventilo1)
+// temps.subscribe(ventilo2)
+// temps.subscribe(ventilo3)
 temps.subscribe(frigo1)
 temps.subscribe(frigo2)
 
-temps.setTemperature(40)
-temps.setTemperature(-5)
-temps.unsubscribe(ventilo2)
-temps.setTemperature(50)
+let temperatureControl = document.querySelector('#temperature') as HTMLInputElement
+temperatureControl.addEventListener('change', (e: Event) => {
+    temps.setTemperature(temperatureControl.value);
+})
+
+let btnAddVentilo = document.querySelector('#add-ventilo') as HTMLButtonElement
+btnAddVentilo.addEventListener("click", (e: Event) => {
+    nbrVentilo++;
+    let newVentilo: observe = new ventilo("ventilo"+nbrVentilo.toString(), temps)
+    temps.subscribe(newVentilo)
+})
--- src/classes/ventilo.ts
+++ src/classes/ventilo.ts
@@ -1,16 +1,47 @@
 import {observe} from '../interfaces/observe';
+import { subject } from '../interfaces/subject';
 
 export class ventilo implements observe {
-     puissance : number = 9000;
-    constructor (private name: string){
-
+    private  puissance: number = 400;
+    private htmlDiv: HTMLDivElement;
+    private htmlButton: HTMLButtonElement;
+    constructor(private name: string, tempsObservable: subject) {
+        let container = document.querySelector("#ventilo-container") as HTMLDivElement
+        let ventiloDiv = document.createElement('div')
+        ventiloDiv.className = "ventilo-div"
+        let div = document.createElement('div')
+        this.htmlButton = document.createElement('button')
+        this.htmlButton.innerText = "Se désabonner"
+        this.htmlButton.addEventListener('click', (e: Event) => {
+            if (this.htmlButton.innerText === "Se désabonner") {
+                this.htmlButton.innerText = "S'abonner"
+                tempsObservable.unsubscribe(this)
+            } else {
+                this.htmlButton.innerText = "Se désabonner"
+                tempsObservable.subscribe(this)
+            }
+        })
+        div.innerText = this.name;
+        div.className = 'ventilo-off'
+        container.append(div)
+        div.id = this.name
+        this.htmlDiv = div
+        ventiloDiv.append(div)
+        ventiloDiv.append(this.htmlButton)
+        container.append(ventiloDiv)
     }
     update (temps: number){
         if (temps > 30 ){
             console.log('Démarrer le ventilo', this.name);
+            // let div = document.querySelector("#"+this.name)
+            this.htmlDiv.className = 'ventilo-on'
         }
         else{
             console.log('Il ne fait pas chaud car le', this.name, 'à démarrer');
+            // let div = document.querySelector('#' + this.name)
+            this.htmlDiv.className = 'ventilo-off'
+            
         }
+
     }
 }
